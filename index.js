2a2a9acc5244b0a54bc3123971fee2ddc73db065
vitorleo80 <vitorleo@hotmail.com>
2018-08-14 19:22:51 +0100

    no jobs, single job and sequence without order done

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const orderedJobs = job => {
+const orderedJobs = jobs => {
 
-    if (!job) return []
+    if (!jobs) return []
+    const jobsList = []
 
+    //Convert string to an object
+    const jobsObj = JSON.parse(jobs)
+
+    for (const job in jobsObj){
+        
+        //Save job dependency in a variable
+        const dependency = jobsObj[job]
+
+        if (!dependency){
+            !jobsList.includes(job) 
+                ? jobsList.push(job) 
+                : null  
+        }
+    }
+        return jobsList
 }
 
 module.exports = {orderedJobs}
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -8,4 +8,17 @@ describe('orderedJobs', () => {
         const actual = orderedJobs(input);
         expect(actual).to.eql([]);
     })
+    it('should get the job if a single job is given', () => {
+        const input = '{"a": ""}';
+        const actual = orderedJobs(input);
+        expect(actual).to.eql(['a']);
+    })
+    it("returns an array of three elements in any order when passed three jobs without dependencies", () => {
+        const input = '{"a": "", "b": "", "c": ""}';
+        const actual = orderedJobs(input);
+        expect(actual).to.have.lengthOf(3);
+        expect(actual).to.contain('a');
+        expect(actual).to.contain('b');
+        expect(actual).to.contain('c');
+    })
 })
